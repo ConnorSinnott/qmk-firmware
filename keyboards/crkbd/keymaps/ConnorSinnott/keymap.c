1f8af3c93664069fcfe31e3636f2d8dc2f76aca4
connorsinnott <connorsinnott@gmail.com>
2025-11-17 20:38:47 -0800

    Make number layer more useful

--- keyboards/crkbd/keymaps/ConnorSinnott/keymap.c
+++ keyboards/crkbd/keymaps/ConnorSinnott/keymap.c
@@ -56,6 +56,7 @@ enum custom_keycodes {
 #define MO_LWR   MO(_LOWER)
 #define MSE_KEY  LT(_MOUSE, KC_SCLN) // LALT and LSFT are added on process_record_user
 #define ADJ_PIP  LT(_ADJUST, KC_BSLS)
+#define NUM_SPC  LT(_NUMBER, KC_SPC)
 
 // Mod taps
 //[max] ########
@@ -68,6 +69,10 @@ enum custom_keycodes {
 #define APP_BACK G(KC_LBRC)
 #define APP_FORW G(KC_RBRC)
 #define DEL_WORD A(KC_BSPC)
+#define CMD_Z    G(KC_Z)
+#define CMD_X    G(KC_X)
+#define CMD_C    G(KC_C)
+#define CMD_V    G(KC_V)
 
 // Toggle Keys
 #define SC_TOGG SENTENCE_CASE_TOGGLE
@@ -81,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       CW_TOGG,   HRM_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, SFT_SFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_ESC, CMD_SPC,  KC_TAB,     KC_ENT,  MO_LWR, MSE_KEY
+                                          KC_LGUI, NUM_SPC,  KC_TAB,     KC_DEL, LWR_ENT, MSE_KEY
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -125,9 +130,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, _______, _______, _______, _______, _______,                      _______,    KC_7,    KC_8,    KC_9, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, SELLINE, SELWBAK, SELWORD, KC_MINS,                      KC_UNDS,    KC_4,    KC_5,    KC_6, _______, _______,
+      _______, _______, SELLINE, SELWBAK, SELWORD, _______,                      _______,    KC_4,    KC_5,    KC_6, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      _______,    KC_1,    KC_2,    KC_3,    KC_0, _______,
+      _______,   CMD_Z,   CMD_X,   CMD_C, _______,   CMD_V,                      _______,    KC_1,    KC_2,    KC_3,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
