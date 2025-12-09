d1a037d6fa4f5b841013c792c0e071e94c4a487a
connorsinnott <connorsinnott@gmail.com>
2025-12-09 09:36:51 -0800

    Reverse mx and choc files

--- keyboards/crkbd/keymaps/ConnorSinnott/config.h
+++ keyboards/crkbd/keymaps/ConnorSinnott/config.h
@@ -49,6 +49,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SELECT_WORD_OS_MAC
 
+#define RGBLIGHT_SLEEP
+
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
     #define RGB_MATRIX_SLEEP // turn off effects when suspended
